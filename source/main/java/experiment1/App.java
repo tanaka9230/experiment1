ac57f7874bbd81c9e891583eb1773520cc169bfd
tanaka9230 <tanaka9230@users.noreply.github.com>
2015-10-09 22:08:58 +0900

    skeletal codes added

---
+++ source/main/java/experiment1/App.java
@@ -0,0 +1,15 @@
+package experiment1;
+
+/**
+ * Hello World!
+ **/
+public class App
+{
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+
+    static String message() {
+        return "Hello World!";
+    }
+}
